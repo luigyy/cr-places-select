794fa389c8644182bd25c52fa79b7c706c346c32
luigyy <luigy.valverde@ucr.ac.cr>
2023-12-11 16:00:20 -0600

    few changes for npm

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
