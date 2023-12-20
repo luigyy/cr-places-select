b4ffcaba70d26c1202d8a966d53151ac12d2b79e
luigyy <luigy.valverde@ucr.ac.cr>
2023-12-19 18:51:13 -0600

    added example folder

---
+++ src/example/Example.tsx
@@ -0,0 +1,10 @@
+import { ProvinceSelect, MunicipalitySelect, DistrictSelect } from "..";
+export default function Example() {
+  return (
+    <>
+      <ProvinceSelect />
+      <MunicipalitySelect />
+      <DistrictSelect />
+    </>
+  );
+}
