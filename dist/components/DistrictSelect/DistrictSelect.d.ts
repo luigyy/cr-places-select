d552319455dd4cad26c9c3c544e9bdd014628068
luigyy <luigy.valverde@ucr.ac.cr>
2024-01-26 19:06:34 -0600

    build

--- dist/components/DistrictSelect/DistrictSelect.d.ts
+++ dist/components/DistrictSelect/DistrictSelect.d.ts
@@ -1,3 +1,8 @@
-import SelectComponentProps from "..";
+interface SelectComponentProps {
+    ContainerClassName?: string;
+    LabelClassName?: string;
+    SelectInputClassName?: string;
+    LabelContent?: string;
+}
 declare const DistrictSelect: (props: SelectComponentProps) => import("react/jsx-runtime").JSX.Element;
 export default DistrictSelect;
--- dist/components/MunicipalitySelect/MunicipalitySelect.d.ts
+++ dist/components/MunicipalitySelect/MunicipalitySelect.d.ts
@@ -1,3 +1,8 @@
-import SelectComponentProps from "..";
+interface SelectComponentProps {
+    ContainerClassName?: string;
+    LabelClassName?: string;
+    SelectInputClassName?: string;
+    LabelContent?: string;
+}
 declare const MunicipalitySelect: (props: SelectComponentProps) => import("react/jsx-runtime").JSX.Element;
 export default MunicipalitySelect;
